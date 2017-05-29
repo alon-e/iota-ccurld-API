2a72a3f26911c16821b8ca2273f8287860f3cabf
alon-e <alon.elmaliah@gmail.com>
2017-05-29 17:10:29 +0300

    added setup.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 
 from app import app
 
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='iota-ccurld-API',
+    version='',
+    packages=['app'],
+    install_requires=['flask'],
+    url='',
+    license='',
+    author='alon',
+    author_email='',
+    description=''
+)
